5f35f531f02f01f0ed28c12e70d0c80c9bd314b0
liondy <michaelliondy05@gmail.com>
2019-09-04 14:41:44 +0700

    continue state class

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,17 @@
  *
  * @author michael
  */
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 public class Main {
-    static int[][] board;
-    static char[] temp;
-    static int i,j;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        board = new int[3][3];
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 3; j++) {
+        int[][] board = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 board[i][j] = sc.nextInt();
             }
         }
+        State s = new State(board);
+        System.out.println(s.isGoal());
     }
 }
--- src/State.java
+++ src/State.java
@@ -10,8 +10,66 @@
  */
 public class State {
     int[][] board;
+    int[][] goalState = {{1,2,3},{4,5,6},{7,8,0}};
     
+    //initial state
     public State(int[][] board){
         this.board = board;
+    }
+    
+    //make frontier based on the empty tile location (x,y)
+    public State[] makeFrontier(State state){
+        int i,j,k,x=0,y=0;
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                if(state.board[i][j]==0){
+                    x=i;
+                    y=j;
+                }
+            }
+        }
+        State[] arrayFrontier;
+        int[][] newBoard = new int[3][3];
+        if((x==0&&(y==0||y==2))||(x==2&&(y==0||y==2))){
+            arrayFrontier = new State[2];
+            if(x==0&&y==0){
+                
+            }
+        }
+        else if(((x==0||x==2)&&y==1)||(x==1&&(y==0||y==2))){
+            arrayFrontier = new State[3];
+        }
+        else{
+            arrayFrontier = new State[4];
+        }
+        for (i = 0; i < arrayFrontier.length; i++) {
+            for(j=0;j<3;j++){
+                for(k=0;k<3;k++){
+                    
+                }
+            }
+        }
+        return arrayFrontier;
+    }
+    
+//    public State move(State state, int x,int y){
+//        state.board
+//        if(x==0&&y==0){
+//            
+//        }
+//    }
+    
+    //check if the state is the goal state
+    public boolean isGoal(){
+        int i,j;
+        boolean same=true;
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                if(this.board[i][j]!=this.goalState[i][j]){
+                    same=false;
+                }
+            }
+        }
+        return same;
     }
 }
