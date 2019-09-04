b493bb187e78e2dafde257602a1cf727eb343147
liondy <michaelliondy05@gmail.com>
2019-09-04 17:16:38 +0700

    add move method, modify state class

--- src/State.java
+++ src/State.java
@@ -9,12 +9,30 @@
  * @author michael
  */
 public class State {
+    State parent;
     int[][] board;
     int[][] goalState = {{1,2,3},{4,5,6},{7,8,0}};
+    int point,x,y;
     
     //initial state
     public State(int[][] board){
         this.board = board;
+        int i,j;
+        for (i=0;i<3;i++) {
+            for(j=0;j<3;j++){
+                if(this.board[i][j]==this.goalState[i][j]){
+                    this.point++;
+                }
+                if(this.board[i][j]==0){
+                    this.x=i;
+                    this.y=j;
+                }
+            }
+        }
+    }
+    
+    public void setParent(State s){
+        this.parent = s;
     }
     
     //make frontier based on the empty tile location (x,y)
@@ -52,12 +70,49 @@ public class State {
         return arrayFrontier;
     }
     
-//    public State move(State state, int x,int y){
-//        state.board
-//        if(x==0&&y==0){
-//            
-//        }
-//    }
+    public State moveUp(State state){
+        if(state.x>0){
+            State newState = new State(state.board);
+            int temp = newState.board[x-1][y];
+            newState.board[x-1][y]=0;
+            newState.board[x][y]=temp;
+            return newState;
+        }
+        return null;
+    }
+    
+    public State moveDown(State state){
+        if(state.x<2){
+            State newState = new State(state.board);
+            int temp = newState.board[x+1][y];
+            newState.board[x+1][y]=0;
+            newState.board[x][y]=temp;
+            return newState;
+        }
+        return null;
+    }
+    
+    public State moveLeft(State state){
+        if(state.y>0){
+            State newState = new State(state.board);
+            int temp = newState.board[x][y-1];
+            newState.board[x][y-1]=0;
+            newState.board[x][y]=temp;
+            return newState;
+        }
+        return null;
+    }
+    
+    public State moveRight(State state){
+        if(state.y<2){
+            State newState = new State(state.board);
+            int temp = newState.board[x][y+1];
+            newState.board[x][y+1]=0;
+            newState.board[x][y]=temp;
+            return newState;
+        }
+        return null;
+    }
     
     //check if the state is the goal state
     public boolean isGoal(){
